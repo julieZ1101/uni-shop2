26dc645067e27b240fa5cf9a3a32b87d60b518b1
julieZ <610882576@qq.com>
2022-12-30 20:31:41 +0800

    完成了首页功能的开发

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
 import Vue from 'vue'
 import App from './App'
+// 导入网络请求的包
+import { $http } from '@escook/request-miniprogram'
+uni.$http = $http  
+
+$http.baseUrl='https://www.uinav.com'
+// 请求拦截器
+$http.beforeRequest = function (options) {
+  // do somethimg...
+  uni.showLoading({
+      title: '数据加载中...',
+    })
+}
+// 响应拦截器
+$http.afterRequest = function () {
+  uni.hideLoading()
+}
+uni.$showMsg=function(title='数据请求失败', duration=1500){
+  uni.showToast({
+    title,
+    duration,
+    icon:'none'
+  })
+}
 
 Vue.config.productionTip = false
 
