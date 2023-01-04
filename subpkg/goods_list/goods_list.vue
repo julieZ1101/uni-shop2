83d0b6ca1199134953692d500a46c21103955a5b
julieZ <610882576@qq.com>
2023-01-04 22:31:06 +0800

    完成了商品列表的开发

--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,22 +1,98 @@
 <template>
-	<view>
-		goods_list
-	</view>
+  <view>
+   <view class="goods-list">
+     <view v-for="(item, i) in goodsList" :key="i" @click="gotoDetail(item)">
+       <!-- 为 my-goods 组件动态绑定 goods 属性的值 -->
+       <my-goods :goods="item"></my-goods>
+     </view>
+   </view> 
+  </view>
+  
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			}
-		},
-		methods: {
-			
-		}
-	}
+  export default {
+    data() {
+      return {
+     queryObj: {
+      query:'',
+      cid:'',
+      pagenum:1,
+      pagesize:10
+    },
+    goodsList:[],
+    total:0,
+    isloading:false
+      }
+    },
+   onLoad(options){
+     console.log(options)
+     this.queryObj.query=options.query ||''
+      this.queryObj.cid=options.cid ||'' 
+      this.getGoodsList()
+   },
+   methods:{
+    async getGoodsList(cb){
+      this.isloading=true
+     const {data: res} = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+     
+      this.isloading=false
+      cb && cb()
+     if(res.meta.status!==200) return uni.$showMsg()
+     this.goodsList= [...this.goodsList,...res.message.goods]
+     this.total= res.message.total
+     },
+     gotoDetail(item){
+       uni.navigateTo({
+         url:'/subpkg/goods_detail/goods_detail?good_id=' + item.goods_id
+       })
+     },
+   onReachBottom() {
+     if(this.queryObj.pagenum *this.queryObj.pagesize>=this.total) return uni.$showMsg('数据加载完成')
+     if(this.isloading) return
+     this.queryObj.pagenum++
+     this.getGoodsList()
+   },
+   onPullDownRefresh() {
+     this.queryObj.pagenum=1
+     this.total=0
+     this.isloading=false
+     this.goodsList=[]
+     this.getGoodsList(()=>uni.stopPullDownRefresh())
+   }
+   
+   },
+   }
 </script>
+<style lang="scss">
+  .goods-item {
+    display: flex;
+    padding: 10px 5px;
+    border-bottom: 1px solid #f0f0f0;
 
-<style>
+    .goods-item-left {
+      margin-right: 5px;
 
+      .goods-pic {
+        width: 100px;
+        height: 100px;
+        display: block;
+      }
+    }
+
+    .goods-item-right {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+
+      .goods-name {
+        font-size: 13px;
+      }
+
+      .goods-price {
+        font-size: 16px;
+        color: #c00000;
+      }
+    }
+  }
 </style>
